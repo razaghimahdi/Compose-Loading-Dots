6b6f12f8d43415b63154b3dd6539448548eb0192
razaghimahdi <razaghimahdi78@gmail.com>
2023-01-29 12:32:26 +0330

    - Updated MainActivity.kt

--- app/src/main/java/com/razzaghi/composeloadingdots/MainActivity.kt
+++ app/src/main/java/com/razzaghi/composeloadingdots/MainActivity.kt
@@ -3,6 +3,8 @@ package com.razzaghi.composeloadingdots
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Easing
+import androidx.compose.animation.core.LinearEasing
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,11 +14,11 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.razzaghi.compose_loading_dots.LoadingBiggy
-import com.razzaghi.compose_loading_dots.LoadingFady
-import com.razzaghi.compose_loading_dots.LoadingWavy
+import com.razzaghi.compose_loading_dots.*
+import com.razzaghi.compose_loading_dots.core.rememberDotsLoadingController
 import com.razzaghi.composeloadingdots.ui.theme.ComposeLoadDotsTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +26,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeLoadDotsTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
@@ -47,11 +48,44 @@ fun SampleLoading() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        LoadingWavy(modifier = Modifier.padding(vertical = 8.dp))
+        val rememberDotsLoadingWavyController = rememberDotsLoadingController()
+        val rememberDotsLoadingFadyController = rememberDotsLoadingController()
+        val rememberDotsLoadingBiggyController = rememberDotsLoadingController()
+        val rememberLoadingScalyController = rememberDotsLoadingController()
+        val rememberLoadingDancingController = rememberDotsLoadingController()
+        val rememberLoadingCircleFadyController = rememberDotsLoadingController()
 
-        LoadingFady(modifier = Modifier.padding(vertical = 8.dp))
 
-        LoadingBiggy(modifier = Modifier.padding(vertical = 8.dp))
+        LoadingWavy(
+            controller = rememberDotsLoadingWavyController,
+            modifier = Modifier.padding(vertical = 8.dp),
+        )
+
+        LoadingFady(
+            controller = rememberDotsLoadingFadyController,
+            modifier = Modifier.padding(vertical = 8.dp),
+        )
+
+        LoadingBiggy(
+            controller = rememberDotsLoadingBiggyController,
+            modifier = Modifier.padding(vertical = 8.dp),
+        )
+
+        LoadingScaly(
+            controller = rememberLoadingScalyController,
+            modifier = Modifier.padding(vertical = 8.dp,),
+        )
+
+        LoadingDancing(
+            controller = rememberLoadingDancingController,
+            modifier = Modifier.padding(vertical = 8.dp),
+        )
+
+        LoadingCircleFady(
+            controller = rememberLoadingCircleFadyController,
+            modifier = Modifier.padding(vertical = 8.dp),
+        )
+
 
     }
 }
