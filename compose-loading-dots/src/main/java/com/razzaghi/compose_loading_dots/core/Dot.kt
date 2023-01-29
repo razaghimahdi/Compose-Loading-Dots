e826a6f1cd546b7aa7c118e0573afcd1e2a876a9
razaghimahdi <razaghimahdi78@gmail.com>
2023-01-29 12:25:10 +0330

    - Updated Dot.kt: added new modifier

--- compose-loading-dots/src/main/java/com/razzaghi/compose_loading_dots/core/Dot.kt
+++ compose-loading-dots/src/main/java/com/razzaghi/compose_loading_dots/core/Dot.kt
@@ -1,4 +1,20 @@
-package com.razzaghi.compose_loading_dots
+/*
+ * Copyright (C) 2022 razaghimahdi (Mahdi Razzaghi Ghaleh)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.razzaghi.compose_loading_dots.core
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.offset
@@ -18,6 +34,7 @@ fun Dot(
     size: Dp = 25.dp,
     color: Color = MaterialTheme.colors.primary,
     yOffset: () -> Float = { 0F },
+    xOffset: () -> Float = { 0F },
     alpha: () -> Float = { 1F }
 ) {
 
@@ -25,7 +42,7 @@ fun Dot(
         modifier = Modifier
             .padding(horizontal = 3.dp)
             .size(size = size)
-            .offset(y = yOffset().dp)
+            .offset(y = yOffset().dp, x = xOffset().dp)
             .alpha(alpha())
     ) {
         drawCircle(
